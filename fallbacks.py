29e13388365c6b8814aa77a4b241ee6b237ee9db
slaus <stanislaussteinvk@gmail.cm>
2021-03-13 15:08:34 +0100

    first commit

---
+++ fallbacks.py
@@ -0,0 +1,26 @@
+import contextlib
+
+
+class QuietFallback:
+    def __getattr__(self, item):
+        return self
+
+    def __call__(self, *args, **kwargs):
+        return self
+
+    def __iter__(self):
+        return iter([])
+
+
+try:
+    with contextlib.redirect_stdout(None):
+        import pygame
+        LOADED_PYGAME = True
+except ModuleNotFoundError:
+    pygame = QuietFallback()
+    LOADED_PYGAME = True
+
+try:
+    from tqdm import tqdm
+except ModuleNotFoundError:
+    tqdm = lambda iterable, *args, **kwargs: iterable
