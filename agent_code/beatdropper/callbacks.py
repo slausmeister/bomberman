17a4dc458b56e4214b9b7d56d872844843b1a925
slaus <stanislaussteinvk@gmail.cm>
2021-03-23 09:18:21 +0100

    A Target lock function; Will probably be removed

--- agent_code/beatdropper/callbacks.py
+++ agent_code/beatdropper/callbacks.py
@@ -30,6 +30,7 @@ def setup(self):
         self.logger.info("Loading model from saved state.")
         with open("my-saved-model.pt", "rb") as file:
             self.model = pickle.load(file)
+    self.target_pos=None
 
 
 def act(self, game_state: dict) -> str:
@@ -41,16 +42,51 @@ def act(self, game_state: dict) -> str:
     :param game_state: The dictionary that describes everything on the board.
     :return: The action to take as a string.
     """
-    # todo Exploration vs exploitation
+    """ # todo Exploration vs exploitation
     random_prob = .1
     if self.train and random.random() < random_prob:
         self.logger.debug("Choosing action purely at random.")
         # 80%: walk in any direction. 10% wait. 10% bomb.
-        return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .1, .1])
+        return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .1, .1]) """
 
     self.logger.debug("Querying model for action.")
-    return np.random.choice(ACTIONS, p=self.model)
 
+    # Look for a target if none is found
+    
+    coin_lock(self,game_state)
+    print(self.target_pos)
+    return game_state['user_input']
+    #return np.random.choice(ACTIONS, p=self.model)
+
+def norm(a,b):
+    return abs(a[0]-b[0])+abs(a[1]-b[1])
+
+def coin_lock(self, game_state):
+    if self.target_pos == None or self.target_pos not in game_state['coins']:
+        coins = game_state['coins']
+
+        coin_distance = [norm(coin,game_state['self'][3]) for coin in coins]
+
+        if len(coin_distance)!=0:
+            self.target_pos = coins[coin_distance.index(min(coin_distance))]
+            print("Found a new target!")
+        else:
+            self.target_pos=None
+            print("No target could be found")
+        
+        
+
+
+
+def relative_position(entity, game_state, second=None):
+    """
+    Returns an array indicating the relative position of two objects
+    
+    """
+    if second == None:
+        return tuple(np.subtract(entity,game_state['self'][3]))
+    else:
+        return tuple(np.subtract(entity,second))
 
 def state_to_features(game_state: dict) -> np.array:
     """
@@ -66,14 +102,20 @@ def state_to_features(game_state: dict) -> np.array:
     :param game_state:  A dictionary describing the current game board.
     :return: np.array
     """
+    
     # This is the dict before the game begins and after it ends
     if game_state is None:
         return None
 
-    # For example, you could construct several channels of equal shape, ...
+# For example, you could construct several channels of equal shape, ...
     channels = []
     channels.append(...)
     # concatenate them as a feature tensor (they must have the same shape), ...
     stacked_channels = np.stack(channels)
-    # and return them as a vector
+
+# and return them as a vector
     return stacked_channels.reshape(-1)
+
+    
+
+    
